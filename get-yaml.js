2f7605b54c100dacc903bdfed1f99f7fe1017be0
Jason Boxman <jasonb@edseek.com>
2021-07-05 01:34:30 -0400

    Walk topics

--- get-yaml.js
+++ get-yaml.js
@@ -1,14 +1,15 @@
+const program = require('commander');
+const fs = require('fs');
 const fsPath = require('path');
 const asciidoctor = require(`asciidoctor`)();
 const yaml = require('js-yaml');
 
-const assemblyFile = process.argv[2];
+const { walkTopics } = require('./lib');
 
 const asciidocOptions = {
   doctype: 'article',
   safe: 'unsafe',
-  sourcemap: true,
-  base_dir: fsPath.dirname(assemblyFile)
+  sourcemap: true
 }
 
 const memoryLogger = asciidoctor.MemoryLogger.create();
@@ -16,28 +17,65 @@ asciidoctor.LoggerManager.setLogger(memoryLogger);
 
 const onlyLang = lang => node => node.getAttribute('language') == lang;
 
-console.log(`Scanning ${assemblyFile}`);
+program
+  //.arguments('')
+  .description('')
+  .option('--topic <path>', 'Optional: Path to _topic_map.yml file')
+  .action(main);
 
-const doc = asciidoctor.loadFile(assemblyFile, asciidocOptions);
-doc.convert();
+function main(options = {}, cmd = {}) {
+  const topicPath = options.topic ? options.topic : fsPath.join(process.cwd(), '_topic_map.yml');
 
-const blocks = doc.findBy({ context: 'listing' }, onlyLang('yaml'));
-if(blocks.length > 0) {
-  for(const block of blocks.sort((a, b) => a.parent.getLineNumber() > b.parent.getLineNumber())) {
-    let o = {};
-    const yamlBlock = block.$source();
-    const { dir, file, path, lineno } = block.parent.source_location;
-    const clean = yamlBlock
-      .replace(/\s?\<[\d\.]+\>\s?$/gm, '')
-      .replace(/\.\.\.\.?/g, '');
+  if(fs.existsSync(topicPath)) {
+    data = fs.readFileSync(topicPath, { encoding: 'utf8' } );
+  }
+  else {
+    process.exit(1);
+  }
 
-    try {
-      o = yaml.load(clean);
-      console.log(`${path} [${lineno}]: OK`)
-    }
-    catch(e) {
-      console.log(`${path} [${lineno}]: Invalid`)
-      console.log(`${e.name} ${e.message}`);
+  const sections = data.split(/---\n/).slice(1);
+  const buckets = sections.map(section => yaml.load(section));
+
+  for(const bucket of buckets) {
+    let doc;
+    if(bucket['Dir'] == 'rest_api') continue;
+    const paths = walkTopics(bucket);
+
+    for(const { path, title } of paths) {
+      const inputPath = fsPath.join(fsPath.dirname(topicPath), `${path}.adoc`);
+      console.log(`Scanning ${inputPath}`);
+
+      try {
+        doc = asciidoctor.loadFile(inputPath, { ...asciidocOptions, base_dir: fsPath.dirname(inputPath) });
+      }
+      catch(e) {
+        console.error(e.message);
+      }
+      doc.convert();
+
+      const blocks = doc.findBy({ context: 'listing' }, onlyLang('yaml'));
+
+      if(blocks.length > 0) {
+        for(const block of blocks.sort((a, b) => a.parent.getLineNumber() > b.parent.getLineNumber())) {
+          let o = {};
+          const yamlBlock = block.$source();
+          const { dir, file, path, lineno } = block.parent.source_location;
+          const clean = yamlBlock
+            .replace(/[ ]*\<[\d\.]+\>[ ]*/g, '')
+            .replace(/\.\.\.\.?/g, '');
+
+          try {
+            o = yaml.load(clean);
+            console.log(`${path} [${lineno}]: OK`)
+          }
+          catch(e) {
+            console.log(`${path} [${lineno}]: Invalid`)
+            console.log(`${e.name} ${e.message}`);
+          }
+        }
+      }
     }
   }
 }
+
+program.parse(process.argv);
