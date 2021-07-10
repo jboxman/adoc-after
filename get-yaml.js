7aa2f0d2af9f5e906589a28bc3f4675619f39c7a
Jason Boxman <jasonb@edseek.com>
2021-07-09 23:04:22 -0400

    Use converted YAML blocks

--- get-yaml.js
+++ get-yaml.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 const fsPath = require('path');
 const asciidoctor = require(`asciidoctor`)();
 const yaml = require('js-yaml');
-//const entities = require('ent');
-const { stripHtml } = require("string-strip-html");
+const entities = require('ent');
+const striptags = require('striptags');
 
 const { walkTopics } = require('./lib');
 
@@ -17,54 +17,11 @@ const asciidocOptions = {
 const memoryLogger = asciidoctor.MemoryLogger.create();
 asciidoctor.LoggerManager.setLogger(memoryLogger);
 
-// This happens prior to include:: expansion, so is not useful.
-asciidoctor.Extensions.register(function() {
-  this.preprocessor(function() {
-    var self = this;
-    self.process(function(doc, reader) {
-      const { lines } = reader;
-      const lines_copy = [ ...lines ];
-      let open = false;
-      let found = false;
-
-      if(lines.length <= 0) return reader;
-
-      // Remove <\d> from every line in every listing block
-      for(let i = 0; i < lines.length; i++) {
-        if(lines[i].substr(0, 4) == '----') {
-          console.log('Found');
-          found = true;
-          open = !open;
-          continue;
-        }
-        if(open) {
-          console.log('Open');
-          console.log(lines_copy[i]);
-          lines_copy[i].replace(/[ ]*\<[\d\.]+\>[ ]*/g, '');
-          lines_copy[i].replace(/\.\.\.\.?/g, '');
-          console.log(lines_copy[i]);
-        }
-      }
-
-      // Missing at least one closing pair, abort.
-      if(found && open) return reader;
-
-      reader.lines = lines_copy;
-      return reader;
-    });
-  });
-});
-
 const onlyLang = lang => node => node.getAttribute('language') == lang;
-
-const stripHtmlOptions = {
-  onlyStripTags: ['i', 'img'],
-  trimOnlySpaces: true
-};
 
 program
   //.arguments('')
-  .description('')
+  .description('Validate YAML listing blocks in Asciidoc files')
   .option('--topic <path>', 'Optional: Path to _topic_map.yml file')
   .action(main);
 
@@ -102,21 +59,31 @@ function main(options = {}, cmd = {}) {
 
       if(blocks.length > 0) {
         for(const block of blocks.sort((a, b) => a.parent.getLineNumber() > b.parent.getLineNumber())) {
-          let o = {};
-          // TODO - $source does NOT resolve attributes, so these can be invalid YAML
-          const yamlBlock = block.getContent();
           const { dir, file, path, lineno } = block.parent.source_location;
-          const clean = yamlBlock;
-            //.replace(/[ ]*\<[\d\.]+\>[ ]*/g, '')
+          const transforms = [
+            // Remove ...
+            s => s.replace(/\.\.\.\.?/g, ''),
+            // Remove (1) style callouts
+            s => s.replace(/[ ]*\([\d\.]+\)[ ]*/g, ''),
+            // Remove <img> and <b> callouts
+            s => striptags(s, { disallowedTags: new Set(['img', 'b']), encodePlaintextTagDelimiters: false })
+          ];
+          const yamlBlock = transforms.reduce((yblock, fn) => fn(yblock), block.getContent());
+          let o = {};
 
-          try {
-            o = yaml.load(clean);
-            console.log(`${path} [${lineno}]: OK`)
-          }
-          catch(e) {
-            console.log(`${path} [${lineno}]: Invalid`)
-            console.log(clean);
-            console.log(`${e.name} ${e.message}`);
+          // Handle multi-YAML document blocks
+          const yamls = yamlBlock.split(/^---$/m);
+
+          for(const partial of yamls) {
+            if(!partial) continue;
+            try {
+              o = yaml.load(entities.decode(partial));
+              console.log(`${path} [${lineno}]: OK`);
+            }
+            catch(e) {
+              console.log(`${path} [${lineno}]: Invalid`);
+              console.log(`${e.name} ${e.message}`);
+            }  
           }
 
         }
